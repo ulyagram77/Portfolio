ff0bcb19b446ae774882efc3002b85e0e50c15ba
Kyrylo Ulianov <ulyak.work@gmail.com>
2025-09-21 22:23:42 +0200

    removed framer motion animations

--- src/components/sections/Tech.jsx
+++ src/components/sections/Tech.jsx
@@ -2,14 +2,12 @@ import { lazy } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { motion } from 'motion/react';
 import PropTypes from 'prop-types';
 
 import { technologies } from '@/constants';
 import { withSectionWrapper } from '@/hoc';
 import { useMatchMedia } from '@/hooks';
 import { styles } from '@/styles';
-import { fadeIn, textVariant } from '@/utils/motion';
 
 const BallCanvas = lazy(() => import('../canvas/Ball.jsx'));
 
@@ -30,18 +28,12 @@ const Tech = withSectionWrapper(() => {
 
   return (
     <>
-      <motion.div
-        variants={textVariant()}
-        className="mb-10 flex flex-col items-center justify-center"
-      >
+      <div className="mb-10 flex flex-col items-center justify-center">
         <p className={styles.sectionSubText}>{t('tech.subtitle')}</p>
         <h2 className={styles.sectionHeadText}>{t('tech.title')}</h2>
-      </motion.div>
+      </div>
 
-      <motion.div
-        variants={fadeIn('bottom', 'spring', 0.5, 0.75)}
-        className="flex flex-row flex-wrap justify-center gap-10"
-      >
+      <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map(technology =>
           !isDesktop ? (
             <TechMobile {...technology} key={technology.name} />
@@ -51,7 +43,7 @@ const Tech = withSectionWrapper(() => {
             </div>
           ),
         )}
-      </motion.div>
+      </div>
     </>
   );
 });
