563d3378fae4bcc4bb092682d5299a9c5526954f
Kyrylo Ulianov <ulyak.work@gmail.com>
2025-09-21 21:26:07 +0200

    removed useless plugin

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 /* eslint-disable no-undef */
-import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
